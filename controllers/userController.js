3bc135fa32ebc2a7731802020fa7f1f9f037f6fa
radityohk <ditoradityo6@gmail.com>
2025-02-09 23:00:16 +0700

    Update : Body parser connect

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,8 +4,11 @@ const { User } = require('../models');
 
 exports.register = async (req, res) => {
     try {
-        let data = req.body;
-        data.password = md5(data.password);
+        let data = {
+            username : req.body.username,
+            password : md5(req.body.password),
+            role : req.body.role
+        };
         const user = await User.create(data);
         res.status(201).json({
             message: 'User registered successfully',
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const userRoutes = require("./routes/userRoutes");
-const siswaRoutes = require("./routes/siswaRoutes");
-const stanRoutes = require("./routes/stanRoutes");
-const menuRoutes = require("./routes/menuRoutes");
-const transaksiRoutes = require("./routes/transaksiRoutes");
-const detailTransaksiRoutes = require("./routes/detail_transaksiRoutes");
-const diskonRoutes = require("./routes/diskonRoutes");
+const userRoutes = require('./routes/userRoutes');
+const siswaRoutes = require('./routes/siswaRoutes');
+const stanRoutes = require('./routes/stanRoutes');
+const menuRoutes = require('./routes/menuRoutes');
+const transaksiRoutes = require('./routes/transaksiRoutes');
+const detailTransaksiRoutes = require('./routes/detail_transaksiRoutes');
+const diskonRoutes = require('./routes/diskonRoutes');
 
-app.use("/users", userRoutes);
-app.use("/siswa", siswaRoutes);
-app.use("/stan", stanRoutes);
-app.use("/menu", menuRoutes);
-app.use("/transaksi", transaksiRoutes);
-app.use("/detail_transaksi", detailTransaksiRoutes);
-app.use("/diskon", diskonRoutes);
+app.use('/users', userRoutes);
+app.use('/siswa', siswaRoutes);
+app.use('/stan', stanRoutes);
+app.use('/menu', menuRoutes);
+app.use('/transaksi', transaksiRoutes);
+app.use('/detail_transaksi', detailTransaksiRoutes);
+app.use('/diskon', diskonRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
