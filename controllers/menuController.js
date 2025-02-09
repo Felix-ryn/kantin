2794e843ad6b3b8217be31308a2c292120e95fcb
radityohk <ditoradityo6@gmail.com>
2025-02-09 22:35:27 +0700

    Update : Controller and Routes

---
+++ controllers/menuController.js
@@ -0,0 +1,50 @@
+const { Menu } = require('../models');
+
+exports.getAll = async (req, res) => {
+    try {
+        const data = await Menu.findAll();
+        res.json({ 
+            message: 'Data retrieved successfully', 
+            data 
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.create = async (req, res) => {
+    try {
+        let data = req.body;
+        const newData = await Menu.create(data);
+        res.status(201).json({ 
+            message: 'Data created successfully', 
+            newData 
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.update = async (req, res) => {
+    try {
+        let data = req.body;
+        const updated = await Menu.update(data, { where: { id: req.params.id } });
+        res.json({ 
+            message: 'Data updated successfully', 
+            updated 
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.delete = async (req, res) => {
+    try {
+        await Menu.destroy({ where: { id: req.params.id } });
+        res.json({ 
+            message: 'Data deleted successfully' 
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
